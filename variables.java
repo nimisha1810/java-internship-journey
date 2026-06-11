62f4acbe36da12e6a74563e6876e0c50914c500a
nimisha1810 <nimishamauryajaipur@gamil.com>
2026-06-11 18:11:22 +0530

    Added variables program

---
+++ variables.java
@@ -0,0 +1,15 @@
+public class variables {
+    public static void main(String[] args) {
+        int age = 18;
+        double height = 5.5;
+        boolean isStudent = true;
+        String name = "Nimisha";
+
+        System.out.println("-----Person Information-----");
+        System.out.println("Name:" + name); 
+        System.out.println("Age: " + age); 
+        System.out.println("Height: " + height + " feet");  
+        System.out.println("Student Status: " + isStudent);
+
+    }
+}
